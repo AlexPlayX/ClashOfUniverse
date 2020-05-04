7c2d9c8c6e5cefcaaa21cd0e3f42a384510e5e4b
Nikita Shalimo 750503 <45921781+Shalimo@users.noreply.github.com>
2020-05-05 00:20:19 +0200

    Add files via upload

---
+++ Clash Of Universe/Assets/Scripts/GameProcess/Bullet3.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet3 : MonoBehaviour
+{
+
+    public Transform p1, p2, p3;
+
+    private float x2, y2, x3, y3;
+
+    private bool enemy=false;
+
+    private float p1ypos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        x2 = p2.position.x;
+        y2= p2.position.y;
+
+        x3 = p3.position.x;
+        y3 = p3.position.y;
+
+        if (this.transform.parent != null)
+            if (this.transform.parent.tag == "bul1" || this.transform.parent.tag == "bul2" || this.transform.parent.tag == "Boss")
+            {
+                enemy = true;
+                p1.tag = "Bullet33";
+                p2.tag = "Bullet33";
+                p3.tag = "Bullet33";
+            }
+            
+
+        this.transform.parent = null;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (enemy)
+        {
+            if (p1 != null)
+            {
+                
+                p1.transform.position += Vector3.down * Time.deltaTime * 9;
+                 
+            }
+
+
+            if (p2 != null)
+            {
+                
+                p2.position = Vector3.MoveTowards(p2.position, new Vector3(x2 - 0.85f, y2 - 12.0f, 0), Time.deltaTime * 7);
+            }
+
+
+            if (p3 != null)
+            {
+                
+                p3.position = Vector3.MoveTowards(p3.position, new Vector3(x2 + 0.85f, y3 - 12.0f, 0), Time.deltaTime * 7);
+            }
+                
+        }
+        else
+        {
+
+            if (p1 != null && p1.tag == "Bullet3")
+                p1.transform.position += Vector3.up * Time.deltaTime * 9;
+
+            if (p2 != null && p2.tag == "Bullet3")
+                p2.position = Vector3.MoveTowards(p2.position, new Vector3(x2 + 3.0f, y2 + 15.0f, 0), Time.deltaTime * 9);
+
+            if (p3 != null && p3.tag == "Bullet3")
+                p3.position = Vector3.MoveTowards(p3.position, new Vector3(x2 - 3.0f, y3 + 15.0f, 0), Time.deltaTime * 9);
+        }
+        
+    }
+
+    private void FixedUpdate()
+    {
+        if (transform.childCount == 0)
+            Destroy(this.gameObject);
+    }
+}
---
+++ Clash Of Universe/Assets/Scripts/GameProcess/Bullet4.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet4 : MonoBehaviour
+{
+
+    public GameObject child;
+    public CircleCollider2D cc;
+    public GameObject explodePrefab;
+    public static bool Enable = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cc.isTrigger = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Enable)
+        {
+            if (this.gameObject != null)
+            {
+                if (transform.childCount == 0)
+                {
+                    cc.isTrigger = false;
+                    transform.position = this.transform.position;
+                }
+                else
+                {
+                    transform.position = child.transform.position;
+                }
+            }
+
+            if ((this.transform.position.y > 5.0f || this.transform.position.y <= -7) && this.gameObject != null)
+                Destroy(this.gameObject);
+        }
+       
+        
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if ((collision.gameObject.tag == "bul1") || (collision.gameObject.tag == "bul2") || (collision.gameObject.tag == "Player") || (collision.gameObject.tag == "Boss"))
+        {
+            if (this.gameObject != null && child == null)
+            {
+                Instantiate(explodePrefab, this.transform.position, this.transform.rotation);
+                Destroy(this.gameObject);
+            }
+        }
+    }
+
+   
+
+}
